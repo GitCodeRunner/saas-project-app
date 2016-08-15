23243413ab65eb24a73267c556faf98c4fe3b41a
recode now <recodenow@gmail.com>
2016-08-15 10:09:25 +0000

    fix authenticate user for user edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,29 @@ class ApplicationController < ActionController::Base
   rescue_from ::Milia::Control::MaxTenantExceeded, :with => :max_tenants
   rescue_from ::Milia::Control::InvalidTenantAccess, :with => :invalid_tenant
 
+
+  def authenticate_tenant!()
+    unless current_user.present? || authenticate_user!(force: true)
+      email = ( params.nil? || params[:user].nil?  ?  "<email missing>"  : params[:user][:email] )
+      flash[:error] = "cannot sign in as #{email}; check email/password"
+      logger.info("MILIA >>>>> [failed auth user] ") unless logger.nil?
+      return false  # abort the before_filter chain
+    end
+
+    trace_tenanting( "authenticate_tenant!" )
+
+    # user_signed_in? == true also means current_user returns valid user
+    raise SecurityError,"*** invalid user_signed_in  ***" unless user_signed_in?
+
+    set_current_tenant   # relies on current_user being non-nil
+
+      # successful tenant authentication; do any callback
+    if self.respond_to?( :callback_authenticate_tenant, true )
+      logger.debug("MILIA >>>>> [auth_tenant callback]")
+      self.send( :callback_authenticate_tenant )
+    end
+
+    true  # allows before filter chain to continue
+  end
+  
 end
